21279684f8e4ee878c8caca74a04c9a919ba71bc
Chris Able <152391628+ChrisKyle-Able@users.noreply.github.com>
2024-06-23 08:56:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const ownervb = process.env.OWNERS || "917605902011;GURU;6281255369012;Ash"
+const ownervb = process.env.OWNERS || "22998438370;𝙲𝙷𝚁𝙸𝚂;917605902011;GURU;6281255369012;Ash"
 
 
 const ownerlist = ownervb.split(';');
@@ -71,9 +71,9 @@ global.APIKeys = {
 // Sticker WM
 global.botname = process.env.BOTNAME
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ'
-global.author = 'ᴳᵘʳᵘ ˢᵉⁿˢᵉⁱ'
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
+global.packname = '👑≛⃞𝘾𝙃𝙍𝙄𝙎༒𝙎𝘼𝙉𝞗⃝⃟⃟¹𐏓꯭'
+global.author = '🌹'
+global.menuvid = 'https://telegra.ph/file/8b77950663655ef8e547d.jpg'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/guru.json\n'
 global.dygp = 'https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT'
